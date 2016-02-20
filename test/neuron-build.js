5a49723a5c76dc874cf6ee8524147ca947143fda
Kael <i@kael.me>
2016-02-20 23:18:50 +0800

    removes the broken tests

--- test/neuron-build.js
+++ test/neuron-build.js
@@ -1,18 +1,4 @@
 'use strict';
 
-var expect = require('chai').expect;
-var neuron_build = require('../');
-
-
-describe("description", function(){
-  it("description", function(done){
-    neuron_build(
-      '/Users/Kael/codes/red/fulishe/static_modules/app-qa', 
-      '/Users/Kael/codes/red/fulishe/static_modules/app-qa2', 
-      function(){
-        console.log(arguments)
-        done()
-      }
-    )
-  });
-});
+var expect = require('chai').expect
+var neuron_build = require('../')
